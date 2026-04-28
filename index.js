0eb62bb06b9bd1f85303f8dda02744d39b328477
z0hra1n <zareena286@gmail.com>
2026-04-28 23:26:30 +0530

    backend v2

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express()
 const axios = require('axios')
 const FormData = require('form-data')
 const fs = require('fs')
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 
+app.use(express.static('public'));
+
+app.post('/verify-art', upload.single('artImage'), (req, res) => {
+    if (!req.file) return res.status(400).json({ error: "No file uploaded" });
+
+    
+
+const fs = require('fs');
 data = new FormData();
-data.append('media', fs.createReadStream('C:\\Users\\IQRA\\Downloads\\ai\\img2.png'))
+data.append('media', fs.createReadStream(req.file.path))
 data.append('models', 'genai')
 data.append('api_user', '3684042');
 data.append('api_secret', 'ooKBRuMvUVZmsvXRZNGKqfsUmnuwhtXJ');
+
+if (!req.file) {
+        return res.status(400).json({ error: "No file uploaded" });
+    }
 
 axios({
     method: 'post',
@@ -17,16 +31,14 @@ data:data,
 headers: data.getHeaders()
 })
 
-.then(function (response){
-
-    console.log(response.data);
+.then(apiRes => {
+    fs.unlinkSync(req.file.path);
+    res.json(apiRes.data);
 })
 
-.catch(function (error) {
-    if(error.response) 
-        console.log(error.response.data);
-    else 
-        console.log(error.message);
+.catch(err =>{
+    res.status(500).json({ error: err.message});
+})
 });
 
 app.get('/', (req, res) => {
